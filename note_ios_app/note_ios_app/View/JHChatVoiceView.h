9f8cd755c41b0bab234049fc3cbf30c971e7db27
[jianghongjohn] <jianghongjohn@163.com>
2017-06-30 17:04:21 +0800

    基础录音页面

---
+++ note_ios_app/note_ios_app/View/JHChatVoiceView.h
@@ -0,0 +1,13 @@
+//
+//  JHChatVoiceView.h
+//  note_ios_app
+//
+//  Created by hyjt on 2017/6/30.
+//  Copyright © 2017年 江弘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JHChatVoiceView : UIView
+
+@end
---
+++ note_ios_app/note_ios_app/View/JHChatVoiceView.m
@@ -0,0 +1,21 @@
+//
+//  JHChatVoiceView.m
+//  note_ios_app
+//
+//  Created by hyjt on 2017/6/30.
+//  Copyright © 2017年 江弘. All rights reserved.
+//
+
+#import "JHChatVoiceView.h"
+
+@implementation JHChatVoiceView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
